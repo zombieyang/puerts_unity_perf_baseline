ba79cbfc7d95d703a8db91f80da21eebd0af5d57
zombieyang <zombieyang@tencent.com>
2022-10-14 21:56:00 +0800

    reduce repeattime

--- unity-project/Assets/Tests/PerformanceTest/_Util.cs
+++ unity-project/Assets/Tests/PerformanceTest/_Util.cs
@@ -5,7 +5,7 @@ using Unity.PerformanceTesting;
 public class PuertsPerformanceUtil
 {
     //public static int repeatCount = 10000;
-    public static int repeatCount = 1000000;
+    public static int repeatCount = 100000;
     public static int warmUpRepeatCount = 100;
     public static int warmUpCount = 1;
     public static int SampleCount = 10;
